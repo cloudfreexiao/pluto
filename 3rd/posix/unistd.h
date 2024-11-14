6d136d16a5563305163a85bcdc1e3a28f9b23ae1
cloudfreexiao <996442717qqcom@gmail.com>
2024-11-14 11:44:39 +0800

    export windows dll

--- 3rd/posix/unistd.h
+++ 3rd/posix/unistd.h
@@ -8,6 +8,12 @@
 #include <assert.h>
 #include <stdlib.h>
 
+#ifdef POSIX_LIBRARY
+    #define POSIX_API __declspec(dllexport)
+#else
+    #define POSIX_API __declspec(dllimport)
+#endif
+
 #define ssize_t size_t
 
 #define random rand
@@ -17,16 +23,16 @@
 
 #define pid_t int
 
-int kill(pid_t pid, int exit_code);
+POSIX_API int kill(pid_t pid, int exit_code);
 
-void usleep(size_t us);
-void sleep(size_t ms);
+POSIX_API void usleep(size_t us);
+POSIX_API void sleep(size_t ms);
 
 enum { CLOCK_THREAD_CPUTIME_ID, CLOCK_REALTIME, CLOCK_MONOTONIC };
-int clock_gettime(int what, struct timespec *ti);
+POSIX_API int clock_gettime(int what, struct timespec *ti);
 
 enum { LOCK_EX, LOCK_NB };
-int flock(int fd, int flag);
+POSIX_API int flock(int fd, int flag);
 
 struct sigaction {
   void (*sa_handler)(int);
@@ -34,24 +40,24 @@ struct sigaction {
   int sa_mask;
 };
 enum { SIGPIPE, SIGHUP, SA_RESTART };
-void sigfillset(int *flag);
-int sigemptyset(int* set);
-void sigaction(int flag, struct sigaction *action, void* param);
+POSIX_API void sigfillset(int *flag);
+POSIX_API int sigemptyset(int* set);
+POSIX_API void sigaction(int flag, struct sigaction *action, void* param);
 
-int pipe(int fd[2]);
-int daemon(int a, int b);
+POSIX_API int pipe(int fd[2]);
+POSIX_API int daemon(int a, int b);
 
 #define O_NONBLOCK 1
 #define F_SETFL 0
 #define F_GETFL 1
 
-int fcntl(int fd, int cmd, long arg);
+POSIX_API int fcntl(int fd, int cmd, long arg);
 
-char *strsep(char **stringp, const char *delim);
+POSIX_API char *strsep(char **stringp, const char *delim);
 
-int write(int fd, const void* ptr, unsigned int sz);
-int read(int fd, void* buffer, unsigned int sz);
-int close(int fd);
+POSIX_API int write(int fd, const void* ptr, unsigned int sz);
+POSIX_API int read(int fd, void* buffer, unsigned int sz);
+POSIX_API int close(int fd);
 
 #define getpid _getpid
 #define open _open
