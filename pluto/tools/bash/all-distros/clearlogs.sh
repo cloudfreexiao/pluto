95b2517f0d8cecbf31d3b56e679d85595b83d4eb
cloudfreexiao <996442717qqcom@gmail.com>
2024-12-17 12:17:38 +0800

    add tools

---
+++ pluto/tools/bash/all-distros/clearlogs.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+sudo find /var/log/* -type f -delete
+
+echo
+echo "Logs have been removed! "
+echo "You may wish to reboot your system now. "
+echo
---
+++ pluto/tools/bash/ubuntu/update.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+sudo apt update
+sudo apt upgrade -y
+sudo apt full-upgrade -y
+
+echo
+echo "Successfully updated the system! "
+echo "You may wish to reboot your system now. "
+echo
