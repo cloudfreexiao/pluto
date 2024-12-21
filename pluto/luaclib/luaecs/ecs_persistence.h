bb7b803e5b3951e3e68d416a493f960abf545c7b
cloudfreexiao <996442717qqcom@gmail.com>
2024-12-21 11:23:28 +0800

    replace ecs lib

--- pluto/luaclib/luaecs/ecs_persistence.h
+++
@@ -1,8 +0,0 @@
-#ifndef LUA_ECS_PERSISTENCE_H
-#define LUA_ECS_PERSISTENCE_H
-
-#include <lua.h>
-
-int lpersistence_methods(lua_State *L);
-
-#endif
--- pluto/luaclib/luaecs/ecs_template.h
+++
@@ -1,8 +0,0 @@
-#ifndef LUA_ECS_TEMPLATE_H
-#define LUA_ECS_TEMPLATE_H
-
-#include <lua.h>
-
-int ltemplate_methods(lua_State *L);
-
-#endif
